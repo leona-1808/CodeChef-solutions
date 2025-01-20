bc711a481a86e9230fe2c0aaf77ce75a193014f3
leona-1808 <148668181+leona-1808@users.noreply.github.com>
2025-01-20 23:31:36 +0530

    Create ex-1.c

---
+++ ex-1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    int hg=a+c;
+    if(hg>=b) printf("YES\n");
+    else printf("NO\n");
+	// your code goes here
+	return 0;
+}
